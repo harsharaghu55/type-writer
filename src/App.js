bba103c58c96a2d90bcd2cb6c0c6194a876b2ef3
harsharaghu55 <harsharaghu55@gmail.com>
2021-06-09 19:00:36 +0530

    the typeing functionality and the auto-foucus on div containg para are added, have to continue with timmer and button functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React,{useState,useEffect} from 'react'
 
 function App() {
+
+  function keyPressed(event){
+    event.preventDefault()
+    if(event.key === para.split("")[count]){
+      setCount(count+1)
+    }
+        
+    if(count === para.length){
+      setCount(0);
+    }
+  }
+  const [para,setPara] = useState("")
+  const [count,setCount] = useState(0);
+
+  useEffect(()=>{
+    setPara("this is the random para shower are test your typeing skill and speed int this area")
+    setCount(0)
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="randomParaShower" tabIndex="1" onKeyPress={keyPressed}>
+        {para && para.split("").map((letter,index)=><span className={index===count ? "letterToType focused":"letterToType"}>{letter}</span>)}
+      </div>
+      <button>
+        reset
+      </button>
+      <button>
+        start
+      </button>
     </div>
   );
 }
